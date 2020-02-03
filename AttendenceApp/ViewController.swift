865f33cad9cbbc244a4c3b1fbd82fd7159de74e3
Kheng Hin <jervisthelolicon@gmail.com>
2020-02-03 16:07:28 +0800

    Initial Commit

---
+++ AttendenceApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AttendenceApp
+//
+//  Created by MAD2_P02 on 3/2/20.
+//  Copyright © 2020 MAD2_P02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
