2674b60429312fa297c81adb4852f98e0d879d52
Attendance <guanliang976@gmail.com>
2020-02-05 16:02:09 +0800

    Random String generator

---
+++ AttendenceApp/RandomString.swift
@@ -0,0 +1,22 @@
+//
+//  RandomString.swift
+//  AttendenceApp
+//
+//  Created by MAD2_P02 on 5/2/20.
+//  Copyright © 2020 MAD2_P02. All rights reserved.
+//
+
+import UIKit
+
+class RandomString: UIViewController {
+
+    @IBOutlet weak var txtRandom: UILabel!
+    @IBAction func btnRandom(_ sender: Any) {
+        let length = 12
+        let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        let randomCharacters = (0..<length).map{_ in characters.randomElement()!}
+        let randomString = String(randomCharacters)
+        txtRandom.text = randomString;
+    }
+    
+}
