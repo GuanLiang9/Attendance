11435a899f3c253f89f13868b321d7a38edad54d
yongfarm <s10189906@connect.np.edu.sg>
2020-02-05 15:31:02 +0800

    1. Added login validation for both Mentor and Student 2. Did login page UI and added identifiers to Teacher and Student storyboard

--- AttendenceApp/ViewController.swift
+++ AttendenceApp/ViewController.swift
@@ -10,11 +10,85 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var userFld: UITextField!
+    @IBOutlet weak var pwdFld: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginTeacher(_ sender: Any) {
+        let appDelegate = (UIApplication.shared.delegate) as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Mentor")
+        let searchString = self.userFld.text
+        let searchstring2 = self.pwdFld.text
+        fetchRequest.predicate = NSPredicate(format:"mentorno = %@", searchString!)
+        do {
+            let result = try managedContext.fetch(fetchRequest)
+            if result.count > 0
+            {
+                let username = (result[0] as AnyObject).value(forKey: "mentorno") as! String
+                let password = (result[0] as AnyObject).value(forKey: "password") as! String
+                if (searchString == username && searchstring2 == password)
+                {
+                    let storyboard = UIStoryboard(name: "Mentor", bundle: nil)
+                    let vc = storyboard.instantiateViewController(withIdentifier: "Mentor") as
+                        UIViewController
+                    vc.modalPresentationStyle = .fullScreen // try without fullscreen
+                    present(vc, animated: true, completion: nil)
+                }
+                else if (searchString == username || searchstring2 == password)
+                {
+                    loginFailAlert()
+                }
+            }
+            else
+            {
+                loginFailAlert()
+            }
+        }
+    }
+    
+    @IBAction func loginStudent(_ sender: Any) {
+        let appDelegate = (UIApplication.shared.delegate) as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Student")
+        let searchString = self.userFld.text
+        let searchstring2 = self.pwdFld.text
+        fetchRequest.predicate = NSPredicate(format:"studentno = %@", searchString!)
+        do {
+            let result = try managedContext.fetch(fetchRequest)
+            if result.count > 0
+            {
+                let username = (result[0] as AnyObject).value(forKey: "studentno") as! String
+                let password = (result[0] as AnyObject).value(forKey: "password") as! String
+                if (searchString == username && searchstring2 == password)
+                {
+                    let storyboard = UIStoryboard(name: "Student", bundle: nil)
+                    let vc = storyboard.instantiateViewController(withIdentifier: "Student") as
+                        UIViewController
+                    vc.modalPresentationStyle = .fullScreen // try without fullscreen
+                    present(vc, animated: true, completion: nil)
+                }
+                else if (searchString == username || searchstring2 == password)
+                {
+                    loginFailAlert()
+                }
+            }
+            else
+            {
+                loginFailAlert()
+            }
+        }
+    }
+    
+    func loginFailAlert(){
+        let alertView = UIAlertController(title:"Invalid User",message: "Please re-enter your login credentials",preferredStyle: UIAlertController.Style.alert)
+        alertView.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {_ in}))
+        self.present(alertView,animated: true,completion: nil)
+    }
 }
 
