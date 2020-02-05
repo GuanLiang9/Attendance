6be8bbe2eca6ffe5ee22e1d0055f0d2624c03151
Ren Kee <renkeerenkee@gmail.com>
2020-02-05 17:40:49 +0800

    Include Various Admin View Controller files

---
+++ AttendenceApp/AddStudentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddStudentViewController.swift
+//  AttendenceApp
+//
+//  Created by ren kee on 5/2/20.
+//  Copyright © 2020 MAD2_P02. All rights reserved.
+//
+
+import UIKit
+
+class AddStudentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
