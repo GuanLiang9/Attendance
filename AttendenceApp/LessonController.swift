1ba1bf196b0f6645a48600a2dad1406655710547
Kheng Hin <jervisthelolicon@gmail.com>
2020-02-07 12:03:14 +0800

    update code to corresponding lesson

--- AttendenceApp/LessonController.swift
+++ AttendenceApp/LessonController.swift
@@ -12,7 +12,7 @@ import CoreData
 
 class LessonController {
     
-    //Retrieve all lessons from Core Data
+    //Retrieve all lessons from Core Data to display in Mentor's timetable
     func retrieveAllLessons()->[Lesson]{
         var lesson:[NSManagedObject] = []
         
@@ -39,5 +39,30 @@ class LessonController {
             print("Could not fetch. \(error), \(error.userInfo)")
         }
         return lessonList
+    }
+    
+    // Add the generated code to corresponding Lesson record in the database
+    func updateLesson(modulename:String, newLesson: Lesson){
+        let appDelegate = (UIApplication.shared.delegate) as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "CDLesson")
+        let lessonmodulename = newLesson.modName
+        fetchRequest.predicate = NSPredicate(format:"modulename = %@", lessonmodulename)
+        
+        do {
+            let result = try managedContext.fetch(fetchRequest)
+            let lessonObj = result[0]
+            
+            lessonObj.setValue(newLesson.uniqueCode, forKey: "uniquecode")
+            lessonObj.setValue(newLesson.codeTimeGen, forKey: "codetimegen")
+            
+            do {
+                try managedContext.save()
+            } catch let error as NSError {
+                print ("Update error: \(error), \(error.userInfo)")
+            }
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
     }
 }
